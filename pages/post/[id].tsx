353e0f1bf60a2c89d2399b08b419e79f4cd41c29
lzb30 <536543269@qq.com>
2020-12-01 22:25:17 +0800

    展示一篇文章

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,5 +1,6 @@
-import { NextPage } from "next";
-import { getPost, getPostIds } from "../../lib/posts";
+import {GetServerSideProps, NextPage} from "next";
+import {getDatabaseConnection} from "../../lib/getDatabaseConnection";
+import {Post} from "../../src/entity/Post";
 
 type Props = {
   post: Post;
@@ -9,26 +10,20 @@ const postsShow: NextPage<Props> = (props) => {
   return (
     <div>
       <h1>{post.title}</h1>
-      <article dangerouslySetInnerHTML={{ __html: post.htmlContent }}></article>
+      <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
     </div>
   );
 };
 
 export default postsShow;
-export const getStaticPaths = async () => {
-  const idList = await getPostIds();
-  return {
-    paths: idList.map((id) => ({ params: { id: id } })),
-    fallback: true,
-  };
-};
 
-export const getStaticProps = async (x: any) => {
-  const id = x.params.id;
-  const post = await getPost(id);
+export const getServerSideProps:GetServerSideProps<any, {id:string}> = async (context)=>{
+  const connection = await getDatabaseConnection()
+  const post = await  connection.manager.findOne(Post,context.params.id)
+  console.log(post)
   return {
-    props: {
-      post: post,
-    },
-  };
-};
+    props:{
+      post:JSON.parse(JSON.stringify(post))
+    }
+  }
+}
