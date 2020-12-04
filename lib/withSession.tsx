a1bdd6ad99856e3eb98c3d0644abe907c3d2c932
liu zhenbin <536543269@qq.com>
2020-12-04 10:37:39 +0800

    使用.env.local设置环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,7 +3,7 @@ import { withIronSession } from "next-iron-session";
 
 export default function withSession(handler: NextApiHandler | GetServerSideProps) {
   return withIronSession(handler, {
-    password: "f09472a6-141e-4bb0-9fec-ed88d2637223",
+    password: process.env.SECRET,
     cookieName: "blog",
     cookieOptions: {
       secure:false
