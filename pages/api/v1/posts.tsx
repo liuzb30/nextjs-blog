48f3dda573e7ee76a0a160074a1fa844b4bda251
liu zhenbin <536543269@qq.com>
2020-12-04 16:54:51 +0800

    成功创建一篇文章

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,11 +1,21 @@
 import { NextApiHandler } from "next";
 import { getPosts } from "lib/posts";
+import withSession from "../../../lib/withSession";
+import { Post } from "../../../src/entity/Post";
+import { getDatabaseConnection } from "../../../lib/getDatabaseConnection";
 
-const posts: NextApiHandler = async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  const posts = await getPosts();
-  res.end(JSON.stringify(posts));
-};
+const posts: NextApiHandler = withSession(async (req, res) => {
+  const { title, content } = req.body;
+  const post = new Post();
+  post.title = title;
+  post.content = content;
+  // @ts-ignore
+  const user = req.session.get("currentUser");
+  console.log(user);
+  post.author = user;
+  const connection = await getDatabaseConnection();
+  await connection.manager.save(post);
+  res.json(post);
+});
 
 export default posts;
