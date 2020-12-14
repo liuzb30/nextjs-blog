f29a40eaf8a9cdafab105837c32fa708babb9752
liu zhenbin <536543269@qq.com>
2020-12-14 17:21:02 +0800

    完善删除功能

--- pages/api/v1/posts/[id].tsx
+++ pages/api/v1/posts/[id].tsx
@@ -1,27 +1,29 @@
-import {NextApiHandler} from "next";
-import {getDatabaseConnection} from "../../../../lib/getDatabaseConnection";
+import { NextApiHandler } from "next";
+import { getDatabaseConnection } from "../../../../lib/getDatabaseConnection";
 import withSession from "../../../../lib/withSession";
 
-const Posts:NextApiHandler = withSession( async (req,res)=>{
-    console.log(req.method)
-    if(req.method==='PATCH'){
-        console.log(111)
-        const connection = await getDatabaseConnection()
-        const {title, content, id} = req.body;
-        console.log(title,content,id)
-        const post:Post = await connection.manager.findOne('Post',id)
-        post.title = title
-        post.content = content
-        // @ts-ignore
-        const user = req.session.get('currentUser')
-        if(!user){
-            res.statusCode = 401
-            res.end()
-            return
-        }
-        await connection.manager.save(post)
-        res.json(post);
+const Posts: NextApiHandler = withSession(async (req, res) => {
+  const connection = await getDatabaseConnection();
+  if (req.method === "PATCH") {
+    const { title, content, id } = req.body;
+    const post: Post = await connection.manager.findOne("Post", id);
+    post.title = title;
+    post.content = content;
+    // @ts-ignore
+    const user = req.session.get("currentUser");
+    if (!user) {
+      res.statusCode = 401;
+      res.end();
+      return;
     }
-})
+    await connection.manager.save(post);
+    res.json(post);
+  } else if (req.method === "DELETE") {
+    const id = req.query.id;
+    const result = await connection.manager.delete("Post", id);
+    res.statusCode = result.affected >= 0 ? 200 : 400;
+    res.end();
+  }
+});
 
-export default Posts
+export default Posts;
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,30 +1,45 @@
-import {GetServerSideProps, GetServerSidePropsContext, NextPage} from "next";
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import { getDatabaseConnection } from "../../lib/getDatabaseConnection";
 import { Post } from "../../src/entity/Post";
 import marked from "marked";
 import withSession from "../../lib/withSession";
 import Link from "next/link";
+import { useCallback } from "react";
+import axios from "axios";
 
 type Props = {
+  id: number;
   post: Post;
   currentUser: User;
 };
 const postsShow: NextPage<Props> = (props) => {
-  const { post,currentUser } = props;
+  const { post, currentUser, id } = props;
+  const onRemove = useCallback(() => {
+    axios.delete(`/api/v1/posts/${id}`).then(
+      (res) => {
+        window.alert("删除成功");
+        location.href = "/post";
+      },
+      () => {
+        window.alert("删除失败");
+      }
+    );
+  }, [id]);
   return (
     <>
       <div className="wrapper">
         <header>
           <h1>{post.title}</h1>
-          {
-            currentUser &&
+          {currentUser && (
             <p>
-              <Link href="/post/[id]/edit" as={`/post/${post.id}/edit`}><a>编辑</a></Link>
+              <Link href="/post/[id]/edit" as={`/post/${post.id}/edit`}>
+                <a>编辑</a>
+              </Link>
+              <button onClick={onRemove}>删除</button>
             </p>
-          }
-
+          )}
         </header>
-          <article
+        <article
           className="markdown-body"
           dangerouslySetInnerHTML={{ __html: marked(post.content) }}
         ></article>
@@ -35,10 +50,13 @@ const postsShow: NextPage<Props> = (props) => {
           margin: 16px auto;
           padding: 0 16px;
         }
-        
+
         h1 {
           padding-bottom: 16px;
           border-bottom: 1px solid #666;
+        }
+        button {
+          margin-left: 10px;
         }
       `}</style>
     </>
@@ -47,16 +65,19 @@ const postsShow: NextPage<Props> = (props) => {
 
 export default postsShow;
 
-export const getServerSideProps: GetServerSideProps<any,{ id: string } > = withSession(
-    async (context:GetServerSidePropsContext) => {
-      const connection = await getDatabaseConnection();
-      const post = await connection.manager.findOne(Post, context.params.id as string);
-      const currentUser = (context.req as any).session.get('currentUser') || null;
-      return {
-        props: {
-          post: JSON.parse(JSON.stringify(post)),
-          currentUser
-        },
-      };
-    }
-)  ;
+export const getServerSideProps: GetServerSideProps<
+  any,
+  { id: string }
+> = withSession(async (context: GetServerSidePropsContext) => {
+  const connection = await getDatabaseConnection();
+  const id = context.params.id as string;
+  const post = await connection.manager.findOne(Post, id);
+  const currentUser = (context.req as any).session.get("currentUser") || null;
+  return {
+    props: {
+      id: parseInt(id),
+      post: JSON.parse(JSON.stringify(post)),
+      currentUser,
+    },
+  };
+});
