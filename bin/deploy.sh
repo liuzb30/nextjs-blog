ae62df21d67788796ac5bc9c2a53257641eb9fd2
liu zhenbin <536543269@qq.com>
2020-12-14 17:49:27 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,8 +3,8 @@ cd /home/blog/app/nextjs-blog &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
-docker build -t fang/node-web-app . &&
+docker build -t lzb/node-web-app . &&
 docker kill app &&
 docker rm app &&
-docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+docker run --name app --network=host -p 3000:3000 -d lzb/node-web-app &&
 echo 'OK!'
