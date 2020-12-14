efae331c7069094a8ddf180be51ae56fa8db1540
lzb30 <536543269@qq.com>
2020-12-15 00:34:25 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ yarn typeorm:build &&
 yarn m:run &&
 yarn build &&
 docker build -t lzb/node-web-app . &&
-docker kill app &&
-docker rm app &&
+docker kill app;
+docker rm app;
 docker run --name app --network=host -p 3000:3000 -d lzb/node-web-app &&
 echo 'OK!'
